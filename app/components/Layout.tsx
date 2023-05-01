1cbf23bc55f2091932e9a3e2bd62c0b619f0d403
Mattia46 <mattia.assogna@gmail.com>
2023-05-01 12:37:28 +0100

    updated Layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,18 +1,23 @@
-import { MdAccountCircle } from 'react-icons/md';
-import { Outlet, Link } from "@remix-run/react";
+import { Outlet, Link, Form } from "@remix-run/react";
 import { Footer } from "@components/Footer";
 
 export const Layout = ({ user }: any) => {
   return (
     <div className="px-4 pb-4 flex w-full flex-col h-screen pt-4 bg-slate-50 sm:px-6 sm:pb-14 sm:pt-8 lg:px-8 lg:pb-12 lg:pt-8">
-      <div className="text-2xl text-slate-500 lg:text-6xl flex justify-between items-center">
-        <Link to="/" className="w-48 lg:w-1/3"><img src="logo.png" alt="logo"/></Link>
+      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+        <Link className="flex items-center" to="/"><img src="../logo.png" width="40" className="mr-4" alt="logo"/>CS Social</Link>
+        { user &&  <p>{user.email}</p> }
         { user
-          ? <Link to="account"><MdAccountCircle /></Link>
-          : <Link to="login"><MdAccountCircle /></Link>
+          ? <Form action="/logout" method="post">
+            <button type="submit"
+              className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600">
+              Logout
+            </button>
+          </Form>
+          : <Link className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600" to="login">Login</Link>
         }
-      </div>
-      <div className="mt-4 lg:mt-12 flex-grow">
+      </header>
+      <div className="flex-grow">
         <Outlet />
       </div>
       <Footer />
