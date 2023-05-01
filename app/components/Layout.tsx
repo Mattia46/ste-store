d8903e74dcce01b4d54a3123bcc9316f866cee35
Mattia46 <mattia.assogna@gmail.com>
2023-05-01 17:19:06 +0100

    removed typecheck

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,12 +1,22 @@
 import { Outlet, Link, Form } from "@remix-run/react";
 import { Footer } from "@components/Footer";
 
+const Links = () => (
+  <>
+    <Link className="hover:bg-blue-200 active:bg-blue-600" to="marrow">Marrow Records</Link>
+    <Link className="hover:bg-blue-200 active:bg-blue-600" to="ritmo">Ritmo del Barrio</Link>
+    <Link className="hover:bg-blue-200 active:bg-blue-600" to="friends">Friends of Cornerman</Link>
+    <Link className="hover:bg-blue-200 active:bg-blue-600" to="events">Events</Link>
+    <Link className="hover:bg-blue-200 active:bg-blue-600" to="about">About</Link>
+  </>
+);
+
 export const Layout = ({ user }: any) => {
   return (
     <div className="px-4 pb-4 flex w-full flex-col h-screen pt-4 bg-slate-50 sm:px-6 sm:pb-14 sm:pt-8 lg:px-8 lg:pb-12 lg:pt-8">
       <header className="flex items-center justify-between mb-8 bg-indigo-400 p-4 text-white">
         <Link className="flex items-center text-black text-2xl" to="/">Cornerman</Link>
-        { user &&  <p>{user.email}</p> }
+        { user && <p className="hidden md:block">{user.email}</p> }
         { user
           ? <Form action="/logout" method="post">
             <button type="submit"
@@ -18,14 +28,13 @@ export const Layout = ({ user }: any) => {
         }
       </header>
       <div className="flex h-full">
-        <div className="flex flex-col w-96 h-full bg-indigo-300">
-          <Link className="hover:bg-blue-200 active:bg-blue-600" to="marrow">Marrow Records</Link>
-          <Link className="hover:bg-blue-200 active:bg-blue-600" to="ritmo">Ritmo del Barrio</Link>
-          <Link className="hover:bg-blue-200 active:bg-blue-600" to="friends">Friends of Cornerman</Link>
-          <Link className="hover:bg-blue-200 active:bg-blue-600" to="events">Events</Link>
-          <Link className="hover:bg-blue-200 active:bg-blue-600" to="about">About</Link>
+        <div className="hidden md:block">
+          <div className="flex flex-col w-96 h-full bg-indigo-300">
+            <Links />
+          </div>
         </div>
-          <Outlet />
+
+        <Outlet />
       </div>
       <Footer />
     </div>
