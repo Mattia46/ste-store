3ff69666a8c6d0c838f1f05d2bac083be3600754
Mattia46 <mattia.assogna@gmail.com>
2023-05-01 13:52:45 +0100

    init only one migration

--- prisma/migrations/20230501120232_init/migration.sql
+++ prisma/migrations/20230501120232_init/migration.sql
@@ -24,6 +24,15 @@ CREATE TABLE "Note" (
     CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "Post" (
+    "slug" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "markdown" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
